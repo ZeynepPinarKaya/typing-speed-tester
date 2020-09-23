ca96378b705c51fbbdae09508fb7f2309095505b
zeyneppinar <zeynepinarkaya@gmail.com>
2020-09-23 18:51:46 +0300

    init

---
+++ script.js
@@ -0,0 +1,28 @@
+const testWrapper = document.querySelector(".test-wrapper");
+const testArea = document.querySelector("#test-area");
+const originText = document.querySelector("#origin-text p").innerHTML;
+const resetButton = document.querySelector("#reset");
+const theTimer = document.querySelector("timer");
+
+// All leading to zero to numbers 9 or below (purely for aesthetics):
+
+
+
+//Run a standard minute/second/hundredths timer: 
+
+
+
+
+//Match the text entered with the provided text on the page: 
+
+
+
+
+//Start the timer:
+
+
+//Reser everything:
+
+
+
+//Event listeners for keyboard and reset button:
